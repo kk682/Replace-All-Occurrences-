e98674ca3c7053c28d60aae4183d93a530ffbfea
kk682 <kk682@njit.edu>
2023-11-09 23:12:59 -0500

    11092023 11:12PM

---
+++ ReplaceAllOccurrences.py
@@ -0,0 +1,15 @@
+originalText="John is a smart student. John does not study for his exams."
+listOfWords=originalText.split()
+print(listOfWords)
+
+info={}
+info["name"]="John"
+info["characteristics"]="smart"
+info["exam"]="does not study"
+
+newText=[]
+for word in words:
+    newText.replace(replacements.get(name="Brian"))
+return"".join(newText)
+print(newText)
+
