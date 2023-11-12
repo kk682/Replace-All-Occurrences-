cb00202ecde2fa09b1b1587f1f7a5d8e3120be43
kk682 <kk682@njit.edu>
2023-11-12 00:56:03 -0500

    11122023 12:55AM

--- ReplaceAllOccurrences.py
+++ ReplaceAllOccurrences.py
@@ -3,15 +3,13 @@ import sys
 import fileinput
 import re
 
+# originalText="John is a smart student. john does not study for his exams."
 originalText = input("originalName")
+# replaceText="brian is a smart student. john does not study for his exams."
 replaceText = input("newName")
 with open ("TextFile.txt", "r") as data:
     filedata = data.read()
 
-# originalText="John is a smart student. john does not study for his exams."
-newText = filedata.replace(originalText, replaceText)
-
-newText = filedata.replace(originalText.lower(), replaceText)
+newText = filedata.replace(originalText, replaceText, 1)
 with open ("TextFile.txt", "w") as data:
     filedata = data.write(newText)
-
